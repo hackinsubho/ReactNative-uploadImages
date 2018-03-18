383854d5355078f02d50ff57910e5183fb68961a
hackinsubho <subho3010@gmail.com>
2018-03-18 23:56:25 +0530

    Update firebase.js

--- components/firebase.js
+++ components/firebase.js
@@ -2,12 +2,12 @@ import React from 'react';
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyClTVN_Xh8dkpQNM3wA_1M_Ib59Q6QcFFo",
-    authDomain: "uploadimage-26cde.firebaseapp.com",
-    databaseURL: "https://uploadimage-26cde.firebaseio.com",
-    projectId: "uploadimage-26cde",
-    storageBucket: "uploadimage-26cde.appspot.com",
-    messagingSenderId: "307457863604"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
    const firebaseApp =firebase.initializeApp(firebaseConfig);
 
